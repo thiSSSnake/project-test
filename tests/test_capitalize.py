fb8591334d75af057e93d168aeddcb648600c27c
Andrey Emelianenko <lin_user@hppavilion>
2023-09-14 17:56:54 +0300

    add new repo & first auto-test

---
+++ tests/test_capitalize.py
@@ -0,0 +1,11 @@
+from capitalize import capitalize
+
+if capitalize('hello') != 'Hello':
+    raise Exception('Функция работает неправильно!')
+
+
+if capitalize('') != '':
+    raise Exception('Функция работает неправильно!')
+
+
+print('Все тесты пройдены.')
